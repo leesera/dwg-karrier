0c8f26c5cc57653bff085b759d0b665b5fc5b87a
Csoyee <ithdi@naver.com>
2017-01-23 15:28:58 +0900

    Apply Code Convention Review 2

--- Roys/app/src/main/java/com/dwg_karrier/roys/ContentView.java
+++ Roys/app/src/main/java/com/dwg_karrier/roys/ContentView.java
@@ -1,6 +1,7 @@
 package com.dwg_karrier.roys;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
@@ -9,6 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Date;
 
 public class ContentView extends AppCompatActivity {
@@ -53,5 +60,64 @@ public class ContentView extends AppCompatActivity {
         finish();
       }
     });
+  }
+  public class MyAsyncTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected void onPreExecute() {
+      super.onPreExecute();
+      ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+      progressBar.setVisibility(View.VISIBLE);
+    }
+
+    // execute in background
+    @Override
+    protected String doInBackground(String... strings) {
+      String contents = "Why this happens";
+      StringBuilder stringBuilder = new StringBuilder();
+      try {
+        URL url = new URL(strings[0]);
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestProperty("Content-Type", "application/json");
+        conn.setRequestProperty("x-api-key", "vr2E65ffhRAZlJTvwcrmq72zPo2wcSZuuLtQ4ITc");
+        // if HTML responseCode is 200, then it's okay
+        if (conn.getResponseCode() == conn.HTTP_OK) {
+          BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+          String line;
+          while ((line = br.readLine()) != null) {
+            stringBuilder.append(line);
+          }
+          br.close();
+        }
+        conn.disconnect();
+      } catch (Exception e) {
+        Log.e("Error", e.getMessage(), e);
+        return null;
+      }
+
+      try {
+        String jsonStr = stringBuilder.toString();
+        JSONObject jsonObject = new JSONObject(jsonStr);
+        contents = jsonObject.getString("content");
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      return contents;
+    }
+
+    // after execution
+    @Override
+    protected void onPostExecute(String response) {
+      super.onPostExecute(response);
+      if (response == null) {
+        response = "No contents";
+      }
+      ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+      progressBar.setVisibility(View.GONE);
+      Log.i("INFO", response);
+      TextView responseView = (TextView) findViewById(R.id.textView);
+      responseView.setText(response);
+    }
   }
 }
