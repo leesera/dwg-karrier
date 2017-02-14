47afbbef40caac6a0e4f558d13d87cc626a837a4
leesera <qseralee@gmail.com>
2017-02-14 15:24:39 +0900

    DB Migration

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request
 from flask_orator import Orator, jsonify
-from orator.orm import belongs_to, has_many
+from orator.orm import belongs_to, has_many, belongs_to_many
 
 # Configuration
 DEBUG = True
@@ -25,18 +25,36 @@ class User(db.Model):
   __fillable__ = ['accessToken', 'email']
   __table__ = 'users'
 
-  @has_many
-  def messages(self):
-    return Message
+  @belongs_to_many(
+       'subscribes',
+       'subscriber_id', 'subscribed_id',
+       with_timestamps=True
+  )
+  def subscribes(self):
+        return Blog
 
 
 class Blog(db.Model):
+  __fillable__ = ['url']
 
-    __fillable__ = ['url']
-
-    @belongs_to
-    def user(self):
+  @belongs_to_many(
+       'subscribers',
+       'subscribed_id', 'subscriber_id',
+       with_timestamps=True
+  )
+  def subscribers(self):
         return User
+
+  @has_many
+  def pages(self):
+    return Page
+
+
+class Page(db.Model):
+  __fillable__ = ['pageUrl']
+  @belongs_to
+  def blogs(self):
+    return Blog 
 
 @app.route('/users', methods=['POST'])
 def create_user():
@@ -44,5 +62,14 @@ def create_user():
 
   return jsonify(user)
 
+
+@app.route('/users/<int:user_id>/recommend', methods=['GET'])
+def get_user_messages(user_id):
+  user = User.find_or_fail(user_id)
+  find_recommend(user)
+  
+  return None 
+
+
 if __name__ == '__main__':
-    app.run()
+  app.run()
